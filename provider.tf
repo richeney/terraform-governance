e101db6223121a77506f58e9e37307c00f92a7ef
Richard Cheney <richeney@microsoft.com>
2019-05-21 16:15:34 +0100

    Added role based assignments

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
-provider "azurerm" {
-    version = "~> 1.27.1"
+provider "azuread" {
+    version = "~> 0.3.1"
+}
 
-    ## subscription_id = "2d31be49-d999-4415-bb65-8aec2c90ba62"
-    ## client_id       = "c8adcaf8-9277-4307-965b-5904ba4afa8d"
-    ## client_secret   = "79c00d05-2288-4abc-bc11-dd7c9edddf9c"
-    ## tenant_id       = "72f988bf-86f1-41af-99ab-2d7cd011db47"
+provider "azurerm" {
+    version = "~> 1.28.0"
 }
---
+++ rbac_assignments.tf
@@ -0,0 +1,43 @@
+/*
+$ az ad group list --output table --query "[?securityEnabled].{name:displayName, description:description, objectId:objectId}"                                                                                                                         Name                    Description                                      ObjectId
+----------------------  -----------------------------------------------  ------------------------------------
+Databricks Admins       Enable Databricks workspace via portal as admin  3ba57833-991b-40a8-8ce8-825a34164ebf
+RBAC Admins             Allowed to create and assign roles               3defc448-c5f8-4dd0-addd-c94ea52341d3
+Network Admins          Admins for the shared services                   4a1451a1-de76-45e5-ac80-e4276591c96b
+Key Vault Secrets       Those with access to update Key Vault secrets    74fa1c03-aeeb-422e-bab3-7d6575407e9c
+Virtual Machine Admins  Admins for the Virtual Machines                  88515d1f-e386-4a23-afcc-78b012f805f9
+*/
+
+data "azuread_group" "network_admins" {
+  name = "Network Admins"
+}
+
+data "azuread_group" "virtual_machine_admins" {
+  name = "Virtual Machine Admins"
+}
+
+resource "azurerm_role_assignment" "network_admins" {
+  principal_id          = "${data.azuread_group.network_admins.id}"
+  role_definition_name  = "Network Contributor"
+  scope                 = "${azurerm_management_group.prod.id}"
+}
+
+resource "azurerm_role_assignment" "virtual_machine_admins" {
+  principal_id          = "${data.azuread_group.virtual_machine_admins.id}"
+  role_definition_name  = "Virtual Machine Contributor"
+  scope                 = "${azurerm_management_group.prod.id}"
+}
+
+// Reuse the same AD groups for both prod and non-prod.  Allow for future split
+
+resource "azurerm_role_assignment" "network_admins" {
+  principal_id          = "${data.azuread_group.network_admins.id}"
+  role_definition_name  = "Network Contributor"
+  scope                 = "${azurerm_management_group.non-prod.id}"
+}
+
+resource "azurerm_role_assignment" "virtual_machine_admins" {
+  principal_id          = "${data.azuread_group.virtual_machine_admins.id}"
+  role_definition_name  = "Virtual Machine Contributor"
+  scope                 = "${azurerm_management_group.non-prod.id}"
+}
